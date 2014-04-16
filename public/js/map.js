207f13b87bded917569823cd0c6722061b0f2adc
ppcheng <ppcheng@uwaterloo.ca>
2014-04-16 16:14:50 -0400

    Fix map.js jshint errors;

--- public/js/map.js
+++ public/js/map.js
@@ -2,10 +2,10 @@ function initialize() {
 	var mapOptions = {
 		zoom: 13,
 		center: new google.maps.LatLng(43.485435, -80.540270)
-	}
+	};
 	var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 	setMarkers(map, locations);
-};
+}
 
 var picks = [
 	['shawermaPlus', 43.474946, -80.524212, 1],
@@ -25,46 +25,46 @@ var locations = [
 
 function setMarkers(map, locations) {
 	var marker = {
-	    url: 'img/pin.png',
-	    size: new google.maps.Size(38, 35)
+        url: 'img/pin.png',
+        size: new google.maps.Size(38, 35)
 	};
 	var pickmarker = {
 		url: 'img/pin2.png',
 		size: new google.maps.Size(38, 35)
 	};
-  	var shape = {
-      	coord: [1, 1, 1, 20, 18, 20, 18 , 1],
-      	type: 'poly'
-  	};
-  	for (var i = 0; i < picks.length; i++) {
-	    var restaurant = picks[i];
-	    var myLatLng = new google.maps.LatLng(restaurant[1], restaurant[2]);
+    var shape = {
+		coord: [1, 1, 1, 20, 18, 20, 18 , 1],
+		type: 'poly'
+	};
+	for (var i = 0; i < picks.length; i++) {
+		var restaurant = picks[i];
+		var myLatLng = new google.maps.LatLng(restaurant[1], restaurant[2]);
 
-	    var image = new google.maps.Marker({
-	        position: myLatLng,
-	        map: map,
-	        icon: pickmarker,
-	        shape: shape,
-	        title: restaurant[0],
-	        zIndex: restaurant[3],
-	        animation: google.maps.Animation.DROP
-	    });
-  	};
-
-  	for (var i = 0; i < locations.length; i++) {
-	    var restaurant = locations[i];
-	    var myLatLng = new google.maps.LatLng(restaurant[1], restaurant[2]);
-
-	    var image = new google.maps.Marker({
-	        position: myLatLng,
-	        map: map,
-	        icon: marker,
-	        shape: shape,
-	        title: restaurant[0],
-	        zIndex: restaurant[3],
-	        animation: google.maps.Animation.DROP
-	    });
+		var image = new google.maps.Marker({
+			position: myLatLng,
+			map: map,
+			icon: pickmarker,
+			shape: shape,
+			title: restaurant[0],
+			zIndex: restaurant[3],
+			animation: google.maps.Animation.DROP
+		});
 	}
-};
+
+	for (var i = 0; i < locations.length; i++) {
+		var restaurant = locations[i];
+		var myLatLng = new google.maps.LatLng(restaurant[1], restaurant[2]);
+
+		var image = new google.maps.Marker({
+			position: myLatLng,
+			map: map,
+			icon: marker,
+			shape: shape,
+			title: restaurant[0],
+			zIndex: restaurant[3],
+			animation: google.maps.Animation.DROP
+		});
+	}
+}
 
 google.maps.event.addDomListener(window, 'load', initialize);
