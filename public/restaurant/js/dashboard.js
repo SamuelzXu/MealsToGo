1c8b72ca3524447dcf798a8ca06358b41633809a
ppcheng <ppcheng@uwaterloo.ca>
2014-05-12 15:43:16 -0400

    Fix angular bug;

---
+++ public/restaurant/js/dashboard.js
@@ -0,0 +1,18 @@
+var dashboardApp = angular.module('dashboardApp',['ngRoute']);
+
+dashboardApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
+	$routeProvider.
+		when('/restaurant/mobilemanager', {
+			templateUrl: '/partial/mobilemanager.html'
+		}).
+		when('/restaurant/history', {
+			templateUrl: '/partial/history.html'
+		}).
+		when('/restaurant/dashboard', {
+			templateUrl: '/partial/main.html'
+		}).
+		when('/restaurant/changepassword', {
+			templateUrl: '/partial/change_pw.html'
+		});
+	$locationProvider.html5Mode(true);
+}]);
