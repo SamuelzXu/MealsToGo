9bd19e4c079d9186ff98c68c89022f9178b4941a
DongZhang <zhangdongopq@gmail.com>
2015-02-14 17:17:52 -0500

    10 sec countdown cancled, remove useless balance card

--- public/js/admin-fun.js
+++ public/js/admin-fun.js
@@ -1,5 +1,5 @@
-var host = "https://pocketask-api.herokuapp.com/";
-// var host = "http://localhost:8080/";
+// var host = "https://pocketask-api.herokuapp.com/";
+var host = "http://localhost:8080/";
 // var host = "https://pocketask-api-test.herokuapp.com/";
 
 function checktoken(){
